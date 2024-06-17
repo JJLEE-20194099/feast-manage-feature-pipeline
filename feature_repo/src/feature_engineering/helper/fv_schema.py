c1ed84526e2a4f7ba70362909d5a9542b259a760
JJLEE-20194099 <long.lt194099@gmail.com>
2024-06-17 16:43:27 +0000

    update fv func

--- feature_repo/src/feature_engineering/helper/fv_schema.py
+++ feature_repo/src/feature_engineering/helper/fv_schema.py
@@ -1,6 +1,7 @@
 from feast import Entity, FeatureView, FileSource, Field
 from feast.types import Float32, Float64, Int64, String, Int32, VALUE_TYPES_TO_FEAST_TYPES
 from feast.value_type import ValueType
+import json
 
 def create_fv_schema(features, feature_type = 'cat'):
     if feature_type == 'num':
@@ -12,3 +13,14 @@ def create_fv_schema(features, feature_type = 'cat'):
                 Field(name=feature, dtype=VALUE_TYPES_TO_FEAST_TYPES[ValueType.INT32])
                 for feature in features
             ]
+
+def get_fv_schema_by_path(path = '/home/long/long/datn-feast/data/featureset/full_version.json'):
+    feature_set = json.load(open(path, 'r'))
+    cat_cols = feature_set['cat_cols']
+    num_cols = feature_set['num_cols']
+
+    cat_fv_schema = create_fv_schema(cat_cols, feature_type='cat')
+    num_fv_schema = create_fv_schema(num_cols, feature_type='num')
+
+
+    return cat_fv_schema + num_fv_schema
