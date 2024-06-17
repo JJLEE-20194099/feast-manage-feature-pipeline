1f8b5c375927d998cbe035658b9e8130fd521040
JJLEE-20194099 <long.lt194099@gmail.com>
2024-06-17 16:45:39 +0000

    update feature definitions

--- feature_repo/definitions.py
+++ feature_repo/definitions.py
@@ -3,12 +3,15 @@ from feast import Entity, FeatureView, FileSource, Field
 from feast.types import Float32, Float64, Int64, String, Int32, VALUE_TYPES_TO_FEAST_TYPES
 from feast.value_type import ValueType
 
+from src.feature_engineering.helper.fv_schema import get_fv_schema_by_path
 from src.constants.file_path import DATA_FEATURE_SET_HCM, DATA_TARGET_HCM
 
 realestate = Entity(
     name="realestate_id",
     value_type=ValueType.STRING,
     description="The ID of the realestate")
+
+hcm_featureset_path = '/home/long/long/datn-feast/data/featureset/full_version.json'
 
 df_source_hcm = FileSource(
     path=DATA_FEATURE_SET_HCM,
@@ -20,12 +23,7 @@ df_hcm_fv = FeatureView(
     name="df_hcm_feature_view",
     ttl=timedelta(days=3),
     entities=[realestate],
-    schema=[
-        Field(name="num_of_bathroom", dtype=VALUE_TYPES_TO_FEAST_TYPES[ValueType.INT32]),
-        Field(name="district", dtype=VALUE_TYPES_TO_FEAST_TYPES[ValueType.INT32]),
-        Field(name="num_of_bedroom", dtype=VALUE_TYPES_TO_FEAST_TYPES[ValueType.INT32]),
-        Field(name="used_area", dtype=VALUE_TYPES_TO_FEAST_TYPES[ValueType.INT32]),
-        ],
+    schema= get_fv_schema_by_path(path = hcm_featureset_path),
     source=df_source_hcm
 )
 
