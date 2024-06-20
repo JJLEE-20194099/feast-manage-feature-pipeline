d7009394c9ed6d52f965635ac7d16e1710c763bf
JJLEE-20194099 <long.lt194099@gmail.com>
2024-06-20 15:21:25 +0000

    add featureset v2

--- create_dataset.py
+++ create_dataset.py
@@ -7,19 +7,24 @@ from feature_repo.src.feature_engineering.helper.fv_schema import get_feast_feat
 
 store = FeatureStore(repo_path="feature_repo/")
 
-HCM_CONFIG = json.load(open('feature_repo/src/config/featureset/full_version.json', encoding='utf-8'))
-feature_dict = json.load(open(HCM_CONFIG['featureset_path'], 'r', encoding='utf-8'))
-features = feature_dict['cat_cols'] + feature_dict['num_cols']
+for path in [
+    'feature_repo/src/config/featureset/full_version.json',
+    'feature_repo/src/config/featureset/hcm_v2.json'
+]:
 
-entity_df = pd.read_parquet(path=HCM_CONFIG['featureset_target_path'])
+    HCM_CONFIG = json.load(open(path, encoding='utf-8'))
+    feature_dict = json.load(open(HCM_CONFIG['featureset_path'], 'r', encoding='utf-8'))
+    features = feature_dict['cat_cols'] + feature_dict['num_cols']
 
-training_data = store.get_historical_features(
-    entity_df=entity_df,
-    features=get_feast_featureset(fv_name=HCM_CONFIG['df_feature_view_name'], features = features)
-)
+    entity_df = pd.read_parquet(path=HCM_CONFIG['featureset_target_path'])
 
-dataset = store.create_saved_dataset(
-    from_=training_data,
-    name=HCM_CONFIG["feast_dataset_name"],
-    storage=SavedDatasetFileStorage(HCM_CONFIG["feast_dataset_path"])
-)
+    training_data = store.get_historical_features(
+        entity_df=entity_df,
+        features=get_feast_featureset(fv_name=HCM_CONFIG['df_feature_view_name'], features = features)
+    )
+
+    dataset = store.create_saved_dataset(
+        from_=training_data,
+        name=HCM_CONFIG["feast_dataset_name"],
+        storage=SavedDatasetFileStorage(HCM_CONFIG["feast_dataset_path"])
+    )
---
+++ feature_repo/gmm_definitions.py
@@ -0,0 +1,47 @@
+from datetime import timedelta
+from feast import Entity, FeatureView, FileSource, Field
+from feast.types import Float32, Float64, Int64, String, Int32, VALUE_TYPES_TO_FEAST_TYPES
+from feast.value_type import ValueType
+import json
+
+from src.feature_engineering.helper.fv_schema import get_fv_schema_by_path
+
+realestate = Entity(
+    name="realestate_id",
+    value_type=ValueType.STRING,
+    description="The ID of the realestate")
+
+
+HCM_CONFIG = json.load(open('src/config/featureset/hcm_v2.json', encoding='utf-8'))
+hcm_featureset_path = HCM_CONFIG['featureset_path']
+
+df_source_hcm = FileSource(
+    path=HCM_CONFIG['featureset_df_path'],
+    timestamp_field="event_timestamp",
+    # created_timestamp_column="created",
+)
+
+df_hcm_fv = FeatureView(
+    name=HCM_CONFIG['df_feature_view_name'],
+    ttl=timedelta(days=3),
+    entities=[realestate],
+    schema= get_fv_schema_by_path(path = hcm_featureset_path),
+    source=df_source_hcm
+)
+
+
+target_source_hcm = FileSource(
+    path=HCM_CONFIG['featureset_target_path'],
+    timestamp_field="event_timestamp",
+    # created_timestamp_column="created",
+)
+
+target_fv = FeatureView(
+    name=HCM_CONFIG['target_feature_view_name'],
+    entities=[realestate],
+    ttl=timedelta(days=100),
+    schema=[
+        Field(name="target", dtype=VALUE_TYPES_TO_FEAST_TYPES[ValueType.FLOAT])
+        ],
+    source=target_source_hcm
+)
