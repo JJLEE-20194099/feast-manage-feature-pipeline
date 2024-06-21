c3fb0cdec1b8a344d1ecadaf8ea6449f6836c928
JJLEE-20194099 <long.lt194099@gmail.com>
2024-06-21 21:58:48 +0000

    update featureset and feast config

--- create_dataset.py
+++ create_dataset.py
@@ -4,36 +4,35 @@ from feast.infra.offline_stores.file_source import SavedDatasetFileStorage
 import json
 import os
 from feature_repo.src.feature_engineering.helper.fv_schema import get_feast_featureset
+from tqdm import tqdm
 
 store = FeatureStore(repo_path="feature_repo/")
 
-for path in [
-    'feature_repo/src/config/featureset/full_version.json',
-    'feature_repo/src/config/featureset/hcm_v2.json',
-    'feature_repo/src/config/featureset/hcm_v3.json',
+for city in ['hn', 'hcm']:
+    for version in tqdm(range(5)):
+        path = f"feature_repo/src/config/featureset/{city}_v{version}.json"
 
-]:
+        CONFIG = json.load(open(path, encoding='utf-8'))
 
-    HCM_CONFIG = json.load(open(path, encoding='utf-8'))
-    feature_dict = json.load(open(HCM_CONFIG['featureset_path'], 'r', encoding='utf-8'))
-    features = feature_dict['cat_cols'] + feature_dict['num_cols']
+        feature_dict = json.load(open(CONFIG['featureset_path'], 'r', encoding='utf-8'))
+        features = feature_dict['cat_cols'] + feature_dict['num_cols']
 
-    entity_df = pd.read_parquet(path=HCM_CONFIG['featureset_target_path'])
+        entity_df = pd.read_parquet(path=CONFIG['featureset_target_path'])
 
-    try:
-        os.remove(HCM_CONFIG["feast_dataset_path"])
-    except:pass
+        try:
+            os.remove(CONFIG["feast_dataset_path"])
+        except:pass
 
-    training_data = store.get_historical_features(
-        entity_df=entity_df,
-        features=get_feast_featureset(fv_name=HCM_CONFIG['df_feature_view_name'], features = features)
-    )
-
-    try:
-        dataset = store.create_saved_dataset(
-            from_=training_data,
-            name=HCM_CONFIG["feast_dataset_name"],
-            storage=SavedDatasetFileStorage(HCM_CONFIG["feast_dataset_path"])
+        training_data = store.get_historical_features(
+            entity_df=entity_df,
+            features=get_feast_featureset(fv_name=CONFIG['df_feature_view_name'], features = features)
         )
-    except:
-        print(f"Existed Dataset - {HCM_CONFIG['feast_dataset_name']}")
+
+        try:
+            dataset = store.create_saved_dataset(
+                from_=training_data,
+                name=CONFIG["feast_dataset_name"],
+                storage=SavedDatasetFileStorage(CONFIG["feast_dataset_path"])
+            )
+        except:
+            print(f"Existed Dataset - {CONFIG['feast_dataset_name']}")
--- feature_repo/definitions.py
+++ feature_repo/definitions.py
@@ -3,6 +3,7 @@ from feast import Entity, FeatureView, FileSource, Field
 from feast.types import Float32, Float64, Int64, String, Int32, VALUE_TYPES_TO_FEAST_TYPES
 from feast.value_type import ValueType
 import json
+from tqdm import tqdm
 
 from src.feature_engineering.helper.fv_schema import get_fv_schema_by_path
 
@@ -11,37 +12,59 @@ realestate = Entity(
     value_type=ValueType.STRING,
     description="The ID of the realestate")
 
-
-HCM_CONFIG = json.load(open('src/config/featureset/full_version.json', encoding='utf-8'))
-hcm_featureset_path = HCM_CONFIG['featureset_path']
-
-df_source_hcm = FileSource(
-    path=HCM_CONFIG['featureset_df_path'],
-    timestamp_field="event_timestamp",
-    # created_timestamp_column="created",
-)
-
-df_hcm_fv = FeatureView(
-    name=HCM_CONFIG['df_feature_view_name'],
-    ttl=timedelta(days=3),
-    entities=[realestate],
-    schema= get_fv_schema_by_path(path = hcm_featureset_path),
-    source=df_source_hcm
-)
+fv_list = []
 
 
-target_source_hcm = FileSource(
-    path=HCM_CONFIG['featureset_target_path'],
-    timestamp_field="event_timestamp",
-    # created_timestamp_column="created",
-)
+for city in ['hn', 'hcm']:
+    for version in tqdm(range(5)):
+        path = f"./src/config/featureset/{city}_v{version}.json"
 
-target_fv = FeatureView(
-    name=HCM_CONFIG['target_feature_view_name'],
-    entities=[realestate],
-    ttl=timedelta(days=100),
-    schema=[
-        Field(name="target", dtype=VALUE_TYPES_TO_FEAST_TYPES[ValueType.FLOAT])
-        ],
-    source=target_source_hcm
-)
+        CONFIG = json.load(open(path, encoding='utf-8'))
+        featureset_path = CONFIG['featureset_path']
+
+
+        fv_list.append(FeatureView(
+            name=CONFIG['df_feature_view_name'],
+            ttl=timedelta(days=3),
+            entities=[realestate],
+            schema= get_fv_schema_by_path(path = featureset_path),
+            source=FileSource(
+            path=CONFIG['featureset_df_path'],
+            timestamp_field="event_timestamp",
+        )
+        ))
+
+
+        fv_list.append(FeatureView(
+            name=CONFIG['target_feature_view_name'],
+            entities=[realestate],
+            ttl=timedelta(days=100),
+            schema=[
+                Field(name="target", dtype=VALUE_TYPES_TO_FEAST_TYPES[ValueType.FLOAT])
+                ],
+            source=FileSource(
+            path=CONFIG['featureset_target_path'],
+            timestamp_field="event_timestamp",
+        )
+        ))
+
+fv0 = fv_list[0]
+fv1 = fv_list[1]
+fv2 = fv_list[2]
+fv3 = fv_list[3]
+fv4 = fv_list[4]
+fv5 = fv_list[5]
+fv6 = fv_list[6]
+fv7 = fv_list[7]
+fv8 = fv_list[8]
+fv9 = fv_list[9]
+fv10 = fv_list[10]
+fv11 = fv_list[11]
+fv12 = fv_list[12]
+fv13 = fv_list[13]
+fv14 = fv_list[14]
+fv15 = fv_list[15]
+fv16 = fv_list[16]
+fv17 = fv_list[17]
+fv18 = fv_list[18]
+fv19 = fv_list[19]
